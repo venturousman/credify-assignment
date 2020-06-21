fd180546366a7ad12aa28e7be527a63d0dc16128
tinhoangnguyen <tinhoang.nguyen@hotschedules.com>
2020-06-21 11:41:39 +0700

    clean code

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -6,9 +6,5 @@ const { update } = require('../actions/user');
 
 // export a function to add/register routes for the app
 module.exports = function (app) {
-    // app.get('/users', getMany);
-    // app.get('/users/:id', getOne);
-    // app.post('/users', validator(userSchema), create);
     app.patch('/users/:id', validator(userSchema), update);
-    // app.delete('/users/:id', delete);
 };
